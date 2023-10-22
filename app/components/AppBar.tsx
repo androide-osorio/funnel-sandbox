6e3086e0856de80ec5ddad2fed9f2cf5cd4e8fc1
Andres Osorio <androideosorio@me.com>
2023-10-22 19:45:17 +0200

    add top bar

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import Image from 'next/image';
+import Image from "next/image";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import { ArrowUpTrayIcon } from "@heroicons/react/24/solid";
 
 type Props = {
-	breadcrumb?: string[];
+  breadcrumb?: string[];
 };
 
 export default function AppBar({ breadcrumb }: Props) {
-	return (
+  return (
     <header className="flex gap-4 justify-between items-center bg-white px-6 py-4 sticky z-10 border-b-gray-100 border-b">
       <Image
         src="/perspective-logo.png"
@@ -15,20 +17,28 @@ export default function AppBar({ breadcrumb }: Props) {
         width={32}
         height={32}
       />
-      <ol className="flex gap-4 flex-1">
+      <ol className="flex gap-3 flex-1 items-baseline">
         {breadcrumb &&
-          breadcrumb.map((crumb, i) =>
-            i === breadcrumb.length - 1 ? (
-              <li key={`breadcrumb-${i}`}>{crumb}</li>
-            ) : (
-              <li key={`breadcrumb-${i}`}>
-                <span>{crumb}</span>
-                <span>&gt;</span>
+          breadcrumb.map((crumb, i) => (
+            <>
+              <li
+                key={`breadcrumb-${i}`}
+                className="font-medium text-lg flex gap-3"
+              >
+                {crumb}
               </li>
-            )
-          )}
+              {i !== breadcrumb.length - 1 ? (
+                <li role="presentation">
+                  <ChevronRightIcon className="w-4 h-4 mt-2" />
+                </li>
+              ) : null}
+            </>
+          ))}
       </ol>
-      <h1 className="text-xl">Funnel Previewer</h1>
+      <button className="rounded-lg bg-blue-500 hover:bg-blue-700 px-4 py-3 text-white flex items-center gap-2">
+        Preview funnel
+        <ArrowUpTrayIcon className="w-4 h-4 inline-block" />
+      </button>
     </header>
   );
-};
+}
--- app/components/PagePreview.tsx
+++ app/components/PagePreview.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import {
 	Block,
 	ButtonBlock,
@@ -12,32 +13,51 @@ type Props = Page & {
 	bgColor?: string;
 };
 
-const TextComponent: React.FC<TextBlock> = ({ text }) => {
-	return <p>{text}</p>;
+const TextComponent: React.FC<TextBlock> = ({ id, text, color, align }) => {
+	return (
+    <p id={id} style={{ color: color, textAlign: align }}>
+      {text}
+    </p>
+  );
 };
 
 const ImageComponent: React.FC<ImageBlock> = ({ src }) => {
-	return <img src={src} alt="image" />;
+	return <img src={src} alt="image" className="rounded-lg" />;
 };
 
 const ListComponent: React.FC<ListBlock> = ({ id, items }) => {
 	return (
-		<ul>
-			{items.map((item, i) => (
-				<li key={`${id}-item-${i}`}>
-					<p>{item.title}</p>
-					<p>{item.description}</p>
-					{item.src}
-				</li>
-			))}
-		</ul>
-	);
+    <ul>
+      {items.map((item, i) => (
+        <li
+          key={`${id}-item-${i}`}
+          className="flex flex-row gap-3 py-5 px-3 mb-5 w-full rounded-md transition-transform transform-gpu hover:-translate-x-1 bg-slate-100"
+        >
+          <Image
+            src={item.src}
+            alt={item.title}
+            className="mb-3"
+            width={40}
+            height={40}
+            priority
+          />
+          <div>
+            <h3 className="text-lg font-medium text-gray-800 text-left">
+              {item.title}
+            </h3>
+            <p>{item.description}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 const ButtonComponent: React.FC<ButtonBlock> = ({ text, color, bgColor }) => {
 	return (
 		<button
-			className={`bg-${bgColor} text-${color} py-2 px-4 rounded`}
+		style={{ backgroundColor: bgColor, color: color }}
+			className={`bg-blue-500 text-white py-4 px-4 rounded`}
 		>
 			{text}
 		</button>
@@ -51,11 +71,11 @@ const BLOCK_MAPPINGS: Record<string, React.ComponentType<any>> = {
 	button: ButtonComponent,
 };
 
-export function PagePreview({ blocks, bgColor = 'red' }: Props) {
+export function PagePreview({ blocks, bgColor = 'white' }: Props) {
 	const mapBlock = (block: Block) => {
 		const Component = BLOCK_MAPPINGS[block.type];
 		return <Component {...block} />;
 	};
 
-	return <div className={`bg-[${bgColor}]`}>{blocks.map((block) => mapBlock(block))}</div>;
+	return <div className="p-12 flex flex-col gap-5 min-h-full" style={{ backgroundColor: bgColor }}>{blocks.map((block) => mapBlock(block))}</div>;
 }
